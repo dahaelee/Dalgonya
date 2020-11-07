b00041d0cb912acc9ee7b58059f118c0901a12fb
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-11-07 18:53:35 +0900

    UI(NoFirst)

---
+++ Assets/Scripts/TutorialMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TutorialMove : MonoBehaviour
+{
+    GameObject player, player2, canvas;
+    public GameObject prefabTutorial;
+    GameObject menu;
+
+    void Awake()
+    {
+        player = GameObject.Find("cat");
+        player2 = GameObject.Find("img");
+        canvas = GameObject.Find("Canvas");
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void TutorialLoad()
+    {
+
+    }
+
+}
